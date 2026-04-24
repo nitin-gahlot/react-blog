841e52aa4b7d1d7642a294802f5464852631368f
nitin-gahlot <nitinkumar195466@gmail.com>
2026-04-24 19:56:09 +0530

    Enhance Bollywood component to display filtered data
    Fetch and display Bollywood data from Data based on category.

--- Bollywood.jsx
+++ Bollywood.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Data from '../Data'
+import { Link } from 'react-router-dom';
 
 const Bollywood = () => {
-  return (
-    <div>Bollywood</div>
-  )
+
+const BollywoodData = Data.filter((item) => item.category === "Bollywood");
+console.log(BollywoodData)
+
+return (
+<div>
+{BollywoodData.map((bollydata) => (
+<>
+<h1>{bollydata.title}</h1>
+<img src={bollydata.img_url} />
+</>
+))}
+</div>
+)
 }
 
 export default Bollywood
